5f1ff4b255fa11163f76eb6897cb81f7db9f3951
Wildan Fadhlulloh <wildanfadh79@gmail.com>
2022-04-02 10:46:06 +0700

    update titik

--- add.php
+++ add.php
@@ -35,8 +35,7 @@
         $mobile = $_POST['mobile'];
         // include database connection file
         include_once("config.php"); // Insert user data into table
-        $result = mysqli_query($mysqli, "INSERT INTO users(name,email,mobile)
-VALUES('$name','$email','$mobile')");
+        $result = mysqli_query($mysqli, "INSERT INTO users(name,email,mobile) VALUES('$name','$email','$mobile')");
         // Show message when user added
         echo "User added successfully. <a href='index.php'>View Users</a>";
     }
